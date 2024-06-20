ff669c1d579fd9e4f1914fd78c4260bf406e806c
BuildTools <unconfigured@null.spigotmc.org>
2024-06-20 16:20:56 +0300

    Voice channel functions The bot can now join voice channels! :D

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
 
 # step 1 bot set up
 intents: discord.Intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
 client = commands.Bot(command_prefix = "#", intents=intents)
@@ -26,8 +27,24 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hello, i am online")
 
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You are not in a voice channel!")
 
-# step 5 running the code
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("I left the voice channel")
+    else:
+        await ctx.send("I am not currently in a voice channel")
+
+
+# Last step - Starting the bot up
 def main() -> None:
     client.run(token=TOKEN)
 
